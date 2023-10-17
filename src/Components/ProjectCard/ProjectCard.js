5ea128f91d48ad05bcc07e55e0a329ef1b447210
khajamdajmeer <khajamdajmeer@gmail.com>
2023-10-17 17:23:11 +0530

    added project 1

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import './ProjectCard.css';
-import img from '../../images/sunrisenew.jpg'
 import giticon from '../../images/github-icon.png';
-import arrowbox from '../../images/arrowbox.png'
+import arrowbox from '../../images/arrowbox.png';
+import carproject from '../../images/carproject.jpeg';
 const ProjectCard = (props) => {
 
 
   const lorem = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis vel pariatur quasi veniam, magni eius aut ab quidem dicta voluptatibus praesentium. In autem quibusdam ipsum tempora voluptas saepe, architecto suscipit."
   
   return (
-    <>
-      <div className="pcard">
-        <div className="headimg">
-            <img src={props.img ? props.img:img} alt="" />
-        </div>
-        <div className="cardbody">
-            <h2>{props.title ? props.title:'No Title'}</h2>
-            <div className="cardbtns">
-              <a className='checkoutbtn' href={props.link ? props.link :'/'} rel='noreferrer' target="_blank">Live Demo &nbsp;<img src={arrowbox} alt="" /></a>
-              <a className='checkoutbtn' href={props.gitlink ? props.gitlink : '#'} rel='noreferrer' target="_blank"> Code &nbsp;<img src={giticon} alt="git" id='giticon'/></a>
-            </div>
-            {/* <strong>
-            Discription
-            </strong> */}
-            <p>{props.discription ? props.discription:lorem}</p>
-        </div>
+   <>
+   <div className="p-container">
+    <div className="p-left">
+      <img src={carproject} alt="" />
+    </div>
+    <div className="p-right">
+      <h2>Car Rental Service</h2>
+      <p>A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.</p>
+      <div className="stackused">
+        <div className="stack"> React </div>
+        <div className="stack">BootStrap</div>
       </div>
-    </>
+      <div className="buttons">
+        <a className='btnicons'><img src={giticon} alt="" /> Code</a>
+        <a className='btnicons'><img src={arrowbox} alt="" /> Live demo</a>
+      </div>
+    </div>
+   </div>
+   
+   </>
   );
 }
 
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,52 +1,19 @@
 import React from 'react';
 import './Projects.css'
 import ProjectCard from '../ProjectCard/ProjectCard';
-import dailynews from '../../images/projectimgs/dailynews.png'
 const Projects = () => {
 
   
 
-const projectsArray = [
-  {
-    Title:'daily News',
-    Link:'/',
-    gitlink:'https://github.com/khajamdajmeer/MyPortfolio',
-    discription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, harum deserunt. Quos consectetur dicta tempora distinctio itaque sit saepe deserunt non. Nesciunt, nisi?",
-    image:dailynews
-  }, {
-    Title:'daily News',
-    Link:'/',
-    gitlink:'https://github.com/khajamdajmeer/MyPortfolio',
-    discription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, harum deserunt. Quos consectetur dicta tempora distinctio itaque sit saepe deserunt non. Nesciunt, nisi?",
-    image:dailynews
-  }, {
-    Title:'daily News',
-    Link:'/',
-    gitlink:'https://github.com/khajamdajmeer/MyPortfolio',
-    discription: "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, harum deserunt. Quos consectetur dicta tempora distinctio itaque sit saepe deserunt non. Nesciunt, nisi?",
-    image:dailynews
-  }, {
-    Title:'daily News',
-    Link:'/',
-    gitlink:'https://github.com/khajamdajmeer/MyPortfolio',
-    discription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, harum deserunt. Quos consectetur dicta tempora distinctio itaque sit saepe deserunt non. Nesciunt, nisi?",
-    image:dailynews
-  },
-]
+
 
 
   return (
     <>
       <h1 className='projectheading' id='projectshead'>My Projects</h1>
      <div className="projects">
-      {projectsArray.map((ele)=>{
-        return(
-          <ProjectCard img={ele.image} title={ele.Title} link={ele.Link} gitlink={ele.gitlink} discription={ele.discription} />
-        )
-      })}
-        <ProjectCard img={dailynews} title='Daily News'/>
-        <div className="App">
-    </div>
+    <ProjectCard/>
+        
      </div>
     </>
   );
