e3b8f33b08c1633a06bafd5926c9ac361ffb81cd
khajamdajmeer <khajamdajmeer@gmail.com>
2023-11-01 13:07:02 +0530

    added the 3rd project

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -3,7 +3,8 @@ import './ProjectCard.css';
 import giticon from '../../images/github-icon.png';
 import arrowbox from '../../images/arrowbox.png';
 import carproject from '../../images/carproject.jpeg';
-import LeetClone from '../../images/projectimgs/LeetClone.jpeg'
+import LeetClone from '../../images/projectimgs/LeetClone.jpeg';
+import MinimalEcomerce from '../../images/projectimgs/MinimalEcomerce.jpeg';
 const ProjectCard = (props) => {
 
 
@@ -45,6 +46,25 @@ const ProjectCard = (props) => {
       <div className="buttons">
         <a className='btnicons' href='https://github.com/khajamdajmeer/LeetFrontEnd.git' target='_blank' rel='noreferrer'><img src={giticon} alt="" /> Code</a>
         <a className='btnicons' href='https://leetcode-clone-s1t3.onrender.com/' target='_blank' rel='noreferrer'><img src={arrowbox} alt="" /> Live demo</a>
+      </div>
+    </div>
+   </div>
+
+
+   <div className="p-container">
+    <div className="p-left">
+      <img src={MinimalEcomerce} alt="" />
+    </div>
+    <div className="p-right">
+      <h2>E-Shopify: A Robust E-Commerce Frontend</h2>
+      <p>"E-Shopify" is a sophisticated and user-friendly e-commerce website with a beautifully designed frontend that offers an extensive range of over 25 high-quality products. Our e-commerce platform is built with the latest web technologies, providing an exceptional shopping experience for customers.</p>
+      <div className="stackused">
+        <div className="stack"> React </div>
+        <div className="stack">Redux</div>
+      </div>
+      <div className="buttons">
+        <a className='btnicons' href='https://github.com/khajamdajmeer/Ecomerce.git' target='_blank' rel='noreferrer'><img src={giticon} alt="" /> Code</a>
+        <a className='btnicons' href='https://minimal-ecomerce-site.onrender.com/' target='_blank' rel='noreferrer'><img src={arrowbox} alt="" /> Live demo</a>
       </div>
     </div>
    </div>
